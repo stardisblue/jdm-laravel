edc41dd90b93e35305616ace9b1bb89d4b468faa
mel1ssa <melissa.mekaoui@gmail.com>
2017-10-05 00:01:44 +0200

    Integration de vue js au projet

--- app/Relation.php
+++ app/Relation.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Relation extends Model
 {
-    //
+    use ElasticquentTrait;
+
+    public $id;
+    public $source_node;
+    public $node;
+    public $type;
+    public $weight;
 }
