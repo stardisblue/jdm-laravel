904afda890605b32cf39c76f1e6623e506810426
stardisblue <stardisblue@gmail.com>
2017-10-11 02:25:22 +0200

    Setting up NodeController

---
+++ app/Http/Controllers/NodeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Support\Facades\Cache;
+use Meliblue\FetchWord;
+use Meliblue\WordParser;
+
+class NodeController extends Controller
+{
+    public function display(string $word)
+    {
+        // afficher un mot
+        //if (Cache::get($word)) {
+        //    $parsed = Cache::get($word);
+        //} else {
+        $response = FetchWord::fetch(utf8_decode($word));
+        $parsed = WordParser::parse($response['out']);
+        // Cache::put($word, $parsed, 60);
+        //}
+
+        $parsed->prepare();
+
+        return view('node.single', ["parsed" => $parsed]);
+    }
+
+    public function card(string $word)
+    {
+        // afficher un mot
+        if (Cache::get($word)) {
+            $parsed = Cache::get($word);
+        } else {
+            $response = FetchWord::fetch(utf8_decode($word));
+            $parsed = WordParser::parse($response['out']);
+            $parsed->prepare();
+            Cache::put($word, $parsed, 60);
+        }
+
+
+        return json_encode($parsed);
+    }
+}
